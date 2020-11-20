399970df600f5b5500144f0cc550e194e748693a
karthickselvendran <selvendrankarthick@gmail.com>
2020-11-20 14:24:34 +0530

    Updates

---
+++ precache-manifest.bed063d5b2cac82490293afeb709ddbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27c29ba3fbe6d149787d1932f080fb2",
+    "url": "/counters/index.html"
+  },
+  {
+    "revision": "c744a079c464b0a8bc2a",
+    "url": "/counters/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "02007666dafa374bfb6c",
+    "url": "/counters/static/js/2.e8281b7b.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/counters/static/js/2.e8281b7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c744a079c464b0a8bc2a",
+    "url": "/counters/static/js/main.ae3c88cf.chunk.js"
+  },
+  {
+    "revision": "e3237f691fd03e7106f7",
+    "url": "/counters/static/js/runtime-main.031b3d19.js"
+  }
+]);
